16fc54d9588bc80dffa1fdf067c2968512529a98
hjg <computer_cka@163.com>
2020-07-29 01:01:05 +0800

    更新属性配置。

--- web-tool/src/main/java/com/hjg/spring/config/MyConfiguration.java
+++ web-tool/src/main/java/com/hjg/spring/config/MyConfiguration.java
@@ -1,14 +1,11 @@
 package com.hjg.spring.config;
 
 import com.hjg.spring.property.SingleRedisMultiProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(SingleRedisMultiProperties.class)
 public class MyConfiguration {
 
-    @Bean
-    SingleRedisMultiProperties singleRedisMultiProperties() {
-        return new SingleRedisMultiProperties();
-    }
 }
