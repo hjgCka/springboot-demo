3acefc605f8984d17fb606cfbe1ca35c4f79f805
hjg <computer_cka@163.com>
2020-08-14 14:39:40 +0800

    修改多余的配置项。

--- mongodb/src/test/java/com/hjg/mongodb/MongodbAppTest.java
+++ mongodb/src/test/java/com/hjg/mongodb/MongodbAppTest.java
@@ -2,16 +2,13 @@ package com.hjg.mongodb;
 
 import com.hjg.mongodb.dao.BookDao;
 import com.hjg.mongodb.vo.Book;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class MongodbAppTest {
 
     @Autowired
--- redis/src/test/java/com/hjg/redis/RedisAppTest.java
+++ redis/src/test/java/com/hjg/redis/RedisAppTest.java
@@ -1,16 +1,14 @@
 package com.hjg.redis;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class RedisAppTest {
 
     @Autowired
