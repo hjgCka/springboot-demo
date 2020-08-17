b8e0abc581e4b4decda5c4e84a952056b3084b35
hjg <computer_cka@163.com>
2020-08-17 20:30:43 +0800

    更新aggregation部分。

--- mongodb/src/test/java/com/hjg/mongodb/MongoAggregateTest.java
+++ mongodb/src/test/java/com/hjg/mongodb/MongoAggregateTest.java
@@ -1,8 +1,11 @@
 package com.hjg.mongodb;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -12,5 +15,12 @@ public class MongoAggregateTest {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Test
+    public void aggregationTest() {
+        //需要AggregateOperation 数组作为参数
+        Aggregation aggregation = Aggregation.newAggregation();
 
+        String collectionName = "";
+        AggregationResults<Object> result = this.mongoTemplate.aggregate(aggregation, collectionName, Object.class);
+    }
 }
