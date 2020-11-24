096c42e110535d5dac12e1da06adddbadb4b1157
hjg <computer_cka@163.com>
2020-11-24 22:24:35 +0800

    添加kafka模块。

---
+++ kafka/src/main/java/com/hjg/kafka/demo/KafkaApplication.java
@@ -0,0 +1,14 @@
+package com.hjg.kafka.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+/**
+ * @description:
+ * @author: hjg
+ * @createdOn: 2020/11/24
+ */
+@EnableKafka
+@SpringBootApplication
+public class KafkaApplication {
+}
