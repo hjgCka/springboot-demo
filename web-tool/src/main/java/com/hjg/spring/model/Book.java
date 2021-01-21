450f1cde5606fb0c9120958fe0071909065f29f4
hjg <computer_cka@163.com>
2021-01-21 09:57:45 +0800

    更新注释。

--- web-tool/src/main/java/com/hjg/spring/model/Book.java
+++ web-tool/src/main/java/com/hjg/spring/model/Book.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 /**
+ * @DateTimeFormat以及@NumberFormat注解都是用在数据绑定，但是在@RequestBody进行序列化时不会被序列化机制所支持。
  * @description:
  * @author: hjg
  * @createdOn: 2021/1/13
@@ -20,6 +21,9 @@ public class Book {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date publishDate = new Date();
 
+    /**
+     * 返回给客户端的值，由默认的formatter即NumberStyleFormatter决定，且没有开放配置属性可以自定义。
+     */
     @NumberFormat(pattern = "###,###.##")
     private BigDecimal price;
 }
